d3402266e1789f91d1dc94fe1356ce0dd34ad17e
Anton Sheihman <anton.sheihman@tufin.com>
2020-08-31 14:44:55 +0300

    Add script to install fd, removed git files

---
+++ scripts/install_fd_8.1.1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://github.com/sharkdp/fd/releases/download/v8.1.1/fd-v8.1.1-arm-unknown-linux-musleabihf.tar.gz
+tar -xvf fd-v8.1.1-arm-unknown-linux-musleabihf.tar.gz
+cd fd-v8.1.1-arm-unknown-linux-musleabihf
+cp ./fd /usr/local/bin/
+cp ./fd.1 /usr/local/share/man/man1/
+mandb
+
+cd ..
+rm -rf fd-v8.1.1-arm-unknown-linux-musleabihf
